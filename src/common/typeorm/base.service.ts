a11431b30bb98ec8fa50d2380740e0def35677fd
Renan Zelaya <renan_zelaya@hotmail.com>
2022-04-18 16:37:25 -0600

    Refactor base service

--- src/common/typeorm/base.service.ts
+++ src/common/typeorm/base.service.ts
@@ -1,17 +1,36 @@
+import { NotImplementedException } from '@nestjs/common';
 import { PinoLogger } from 'nestjs-pino';
-import {
-  DeepPartial,
-  DeleteResult,
-  FindConditions,
-  FindManyOptions,
-  FindOneOptions,
-  Repository,
-  UpdateResult,
-} from 'typeorm';
+import { DeepPartial, DeleteResult, FindManyOptions, Repository, UpdateResult } from 'typeorm';
 
 import { PaginatedResponse } from '../pagination/paginated-response';
+import { UnknownObject } from '../types';
 
-export class BaseService<TEntity, TRepository extends Repository<TEntity>, TId extends string | number = string> {
+export interface IBaseService<
+  TEntity,
+  TId extends string | number = string,
+  TGetAll = UnknownObject,
+  TGetById = UnknownObject,
+> {
+  create(entity: DeepPartial<TEntity>): Promise<TEntity>;
+  update(id: TId, entity: TEntity): Promise<UpdateResult>;
+  remove(id: TId): Promise<DeleteResult>;
+  findAllPaginated(
+    currentPage: number,
+    take: number,
+    options?: FindManyOptions<TEntity>,
+  ): Promise<PaginatedResponse<TEntity>>;
+  getAll(options?: TGetAll): Promise<TEntity[]>;
+  getById(options?: TGetById): Promise<TEntity | null>;
+}
+
+export abstract class BaseService<
+  TEntity,
+  TRepository extends Repository<TEntity>,
+  TId extends string | number = string,
+  TGetAll = UnknownObject,
+  TGetById = UnknownObject,
+> implements IBaseService<TEntity, TId, TGetAll, TGetById>
+{
   constructor(
     protected readonly logger: PinoLogger,
     protected readonly repository: TRepository,
@@ -20,16 +39,16 @@ export class BaseService<TEntity, TRepository extends Repository<TEntity>, TId e
     this.logger.setContext(loggerContext);
   }
 
-  create(entity: DeepPartial<TEntity>): Promise<TEntity> {
+  async create(entity: DeepPartial<TEntity>): Promise<TEntity> {
     return this.repository.save(entity);
   }
 
-  findAll(options?: FindManyOptions<TEntity>): Promise<TEntity[]> {
-    return this.repository.find(options);
+  async update(id: TId, entity: TEntity): Promise<UpdateResult> {
+    return this.repository.update(id, entity);
   }
 
-  findAllAndCount(options?: FindManyOptions<TEntity>): Promise<[TEntity[], number]> {
-    return this.repository.findAndCount(options);
+  async remove(id: TId): Promise<DeleteResult> {
+    return this.repository.delete(id);
   }
 
   async findAllPaginated(
@@ -37,7 +56,7 @@ export class BaseService<TEntity, TRepository extends Repository<TEntity>, TId e
     take: number,
     options?: FindManyOptions<TEntity>,
   ): Promise<PaginatedResponse<TEntity>> {
-    const [data, count] = await this.findAllAndCount({
+    const [data, count] = await this.repository.findAndCount({
       ...(options || {}),
       take,
       skip: (currentPage - 1) * take,
@@ -53,26 +72,11 @@ export class BaseService<TEntity, TRepository extends Repository<TEntity>, TId e
     };
   }
 
-  findOne(paramOne: TId | FindConditions<TEntity>, options?: FindOneOptions<TEntity>): Promise<TEntity | undefined> {
-    if (typeof paramOne === 'string' || typeof paramOne === 'number') {
-      return this.repository.findOne(paramOne, options);
-    }
-    return this.repository.findOne(paramOne as FindConditions<TEntity>, options);
+  getAll(): Promise<TEntity[]> {
+    throw new NotImplementedException();
   }
 
-  findOneOrFail(options?: FindOneOptions<TEntity>): Promise<TEntity> {
-    return this.repository.findOneOrFail(options);
-  }
-
-  findById(id: TId, options?: FindOneOptions<TEntity>): Promise<TEntity> {
-    return this.repository.findOneOrFail(id, options);
-  }
-
-  update(id: TId, entity: TEntity): Promise<UpdateResult> {
-    return this.repository.update(id, entity);
-  }
-
-  remove(id: TId): Promise<DeleteResult> {
-    return this.repository.delete(id);
+  getById(): Promise<TEntity> {
+    throw new NotImplementedException();
   }
 }
