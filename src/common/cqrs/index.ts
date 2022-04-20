f0da33860f71e3c69128af04872d9b0a81165f61
Renan Zelaya <renan_zelaya@hotmail.com>
2022-04-20 01:39:18 -0600

    Export created interfaces

--- src/common/cqrs/index.ts
+++ src/common/cqrs/index.ts
@@ -1,4 +1,6 @@
 export { BaseExecutedByCommand } from './base-executed-by.command';
 export { BaseExecutedByEvent } from './base-executed-by.event';
+export { BaseExecutedByQuery } from './base-executed-by.query';
 export { BaseCommandHandler, IBaseCommandHandler } from './base.command.handler';
 export { BaseEventHandler, IBaseEventHandler } from './base.event.handler';
+export { BaseQueryHandler, IBaseQueryHandler } from './base.query.handler';
