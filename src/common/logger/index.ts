eb0a2a18c81f000768dcd1fea4b84eb13cce76c9
Renan Zelaya <renan_zelaya@hotmail.com>
2023-03-17 15:34:34 -0600

    Refactor logger methods

--- src/common/logger/index.ts
+++ src/common/logger/index.ts
@@ -1,6 +1,16 @@
 import { randomUUID } from 'crypto';
+import { Request, Response } from 'express';
 
-export const genReqId = (): string => randomUUID();
+export const genReqId = (req: Request, res: Response): string => {
+  let id = req.get('X-Request-Id');
+
+  if (id) return id;
+
+  id = randomUUID();
+  res.header('X-Request-Id', id);
+
+  return id;
+};
 
 export const timestampFn = (): string => {
   const now = Date.now();
